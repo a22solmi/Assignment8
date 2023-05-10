7197e808d664081e89dd2030337f332243594b18
Sol <a22solmi@student.his.se>
2023-05-10 10:02:59 +0200

    Moved picture to asset folder. Load and resize with Picasso library

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.image_view);
+        Picasso.get()
+                .load("file:///android_asset/android.png")
+                .resize(1000,1000)
+                .into(imageView);
     }
 }
